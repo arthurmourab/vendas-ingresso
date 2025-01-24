d3058cfcaf074f735e497636b9745c109e05adc0
Arthur Bernardo <arthur.bernardo@valori.com.vc>
2025-01-24 01:29:23 -0300

    project start

---
+++ db.sql
@@ -0,0 +1,58 @@
+use tickets;
+
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `created_at` TIMESTAMP NOT NULL,
+    PRIMARY KEY (`id`))
+    ENGINE = InnoDB
+
+
+CREATE TABLE IF NOT EXISTS `partners` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `company_name` VARCHAR(255) NOT NULL,
+    `created_at` TIMESTAMP NOT NULL,
+    `user_id` INT NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `fk_partners_users_idx` (`user_id` ASC) VISIBLE,
+    CONSTRAINT `fk_partners_users`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+    ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `customers` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `address` VARCHAR(255) NOT NULL,
+    `phone` VARCHAR(45) NOT NULL,
+    `created_at` TIMESTAMP NOT NULL,
+    `user_id` INT NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `fk_customers_users1_idx` (`user_id` ASC) VISIBLE,
+    CONSTRAINT `fk_customers_users1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+    ENGINE = InnoDB
+
+
+CREATE TABLE IF NOT EXISTS `events` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `description` VARCHAR(255) NOT NULL,
+    `date` VARCHAR(255) NOT NULL,
+    `location` VARCHAR(255) NOT NULL,
+    `created_at` TIMESTAMP NOT NULL,
+    `partners_id` INT NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `fk_events_partners1_idx` (`partners_id` ASC) VISIBLE,
+    CONSTRAINT `fk_events_partners1`
+    FOREIGN KEY (`partners_id`)
+    REFERENCES `partners` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+    ENGINE = InnoDB
---
+++ src/app.ts
@@ -0,0 +1,48 @@
+import express from 'express'
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({message: 'Hello World!'})
+});
+
+app.post('/auth/login', (req, res) => {
+    const { email, password } = req.body;
+    console.log(email, password);
+    res.send();
+});
+
+app.post('/partners', (req, res) => {
+    const { name, email, password, company_name } = req.body;
+});
+
+app.post('/customers', (req, res) => {
+    const { name, email, password, address, phone } = req.body;
+});
+
+app.post('/partners/events', (req, res) => {
+    const { name, description, date, location } = req.body;
+});
+
+app.get('/partners/events/:eventId', (req, res) => {
+    const { eventId } = req.params;
+    console.log(eventId);
+    res.send();
+});
+
+app.post('/events', (req, res) => {
+    const { name, description, date, location } = req.body;
+});
+
+app.get('/events/:eventId', (req, res) => {
+    const { eventId } = req.params;
+    console.log(eventId);
+    res.send();
+});
+
+
+app.listen(3000, () => {
+    console.log('Running in http://localhost:3000')
+})
